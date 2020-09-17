bfcf8169800996a250c3ca3857024b17d5fe052b
Himanshu Bhavsar <himanshubhavsar1998@gmail.com>
2020-09-17 22:38:40 +0530

    Table Structures for DB

---
+++ DB_Tables_Structure/staff.sql
@@ -0,0 +1,15 @@
+/*
+SQLyog Ultimate v11.33 (32 bit)
+MySQL - 5.0.24-community-nt 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `staff` (
+	`id` int (255),
+	`name` varchar (765),
+	`phone` varchar (765),
+	`dept` varchar (765),
+	`cabin` varchar (765),
+	`attendance` tinyint (1)
+); 
---
+++ DB_Tables_Structure/visitor.sql
@@ -0,0 +1,15 @@
+/*
+SQLyog Ultimate v11.33 (32 bit)
+MySQL - 5.0.24-community-nt 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `visitor` (
+	`visitor_id` int (11),
+	`name` varchar (765),
+	`purpose` varchar (765),
+	`date` date ,
+	`time` time ,
+	`staff_id` int (255)
+); 
